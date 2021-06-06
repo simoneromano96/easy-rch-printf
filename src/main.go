5ecc1f526c49ccac8edb86f54725c27e341625fd
Simone Romano <simoneromano@tutanota.de>
2021-06-06 14:41:59 +0200

    cleanup non-interesting code

--- src/main.go
+++ src/main.go
@@ -12,9 +12,9 @@ func main() {
 	nc, _ := nats.Connect(nats.DefaultURL)
 
 	// Create JetStream Context
-	js, _ := nc.JetStream(nats.PublishAsyncMaxPending(256))
+	js, _ := nc.JetStream(nats.PublishAsyncMaxPending(1024))
 
-	// $ nats str add ORDERS --subjects "ORDERS.*" --ack --max-msgs=-1 --max-bytes=-1 --max-age=1y --storage file --retention limits --max-msg-size=-1 --discard=old
+	// nats str add ORDERS --subjects "ORDERS.*" --ack --max-msgs=-1 --max-bytes=-1 --max-age=1y --storage file --retention limits --max-msg-size=-1 --discard=old
 	si, err := js.AddStream(&nats.StreamConfig{
 		Name:     "ORDERS",
 		Subjects: []string{"ORDERS.*"},
@@ -24,50 +24,26 @@ func main() {
 	}
 	fmt.Println(si)
 
-	// Simple Stream Publisher
-	pack, err := js.Publish("ORDERS.scratch", []byte("hello"))
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(pack)
-
 	// Simple Async Stream Publisher
 	for i := 0; i < 500; i++ {
-		js.PublishAsync("ORDERS.scratch", []byte("hello"))
+		js.PublishAsync("ORDERS.scratch", []byte(fmt.Sprintf("order %d", i)))
 	}
 	select {
 	case <-js.PublishAsyncComplete():
+		fmt.Println("Resolved publish")
 	case <-time.After(5 * time.Second):
 		fmt.Println("Did not resolve in time")
 	}
 
 	// Simple Async Ephemeral Consumer
-	js.Subscribe("ORDERS.*", func(m *nats.Msg) {
+	sub, err := js.Subscribe("ORDERS.*", func(m *nats.Msg) {
 		fmt.Printf("Received a JetStream message: %s\n", string(m.Data))
 	})
 
-	// Simple Sync Durable Consumer (optional SubOpts at the end)
-	sub, err := js.SubscribeSync("ORDERS.*", nats.Durable("MONITOR"), nats.MaxDeliver(3))
 	if err != nil {
 		panic(err)
 	}
 
-	m, err := sub.NextMsg(time.Duration(10) * time.Second)
-	fmt.Println(m)
-	if err != nil {
-		panic(err)
-	}
-
-	// Simple Pull Consumer
-	sub, err = js.PullSubscribe("ORDERS.*", "MONITOR")
-	if err != nil {
-		panic(err)
-	}
-	msgs, err := sub.Fetch(10)
-	fmt.Println(msgs)
-	if err != nil {
-		panic(err)
-	}
 	// Unsubscribe
 	sub.Unsubscribe()
 
